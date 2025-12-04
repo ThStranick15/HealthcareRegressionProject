a8260d8b05e12291b0a70fd10c98d8f5ff2f775b
ThStranick15 <thstranick@gmail.com>
2025-12-03 20:12:14 -0500

    model does not work with interactions

--- DataCleaning.R
+++ DataCleaning.R
@@ -17,7 +17,8 @@ premium_data <- survey_tibble %>%
   filter(!is.na( HICOSTR1_A ) & !HICOSTR1_A %in% invalid_premium) 
 
 #selecting all covariates
-premium_data <- premium_data %>% select(AGEP_A, SEX_A, RACEALLP_A, HISPALLP_A, MARITAL_A, NATUSBORN_A, ORIENT_A, #demographic
+premium_data <- premium_data %>% select(HICOSTR1_A, #response y
+                                        AGEP_A, SEX_A, RACEALLP_A, HISPALLP_A, MARITAL_A, NATUSBORN_A, ORIENT_A, #demographic
                                         POVRATTC_A, RATCAT_A, EDUCP_A, EMDOCCUPN2_A, EMDOCCUPN1_A, ACCSSINT_A, EMDINDSTN1_A, #socio-economic
                                         DIBEV_A, PREDIB_A, GESDIB_A, HYPEV_A, ASEV_A, LONGCOVD2_A, EVERCOVD_A, ANXFREQ_A,DEPFREQ_A, #health conditions
                                         SMOKELSEV1_A, SMKCIGST_A, DRKLIFE_A, DRK12MWK_A, DRKSTAT_A, MODFREQW_A, #health related behaviors
@@ -25,10 +26,7 @@ premium_data <- premium_data %>% select(AGEP_A, SEX_A, RACEALLP_A, HISPALLP_A, M
                                         MCPART_A, MCCHOICE_A, EXCHANGE_A, PLNWRKR1_A, #access to care
                                         PPSU, PSTRAT, WTFA_A)#survey design
 
-#interactions
-
-#EMDOCCUPN2_A,EMDOCCUPN1_A,EMDINDSTN1_A
-#on EMPLASTWK_A=1 or EMPNOWRK_A=1 or EMPWHYNOT_A=7 or EMPWHENWRK_A=1
+#easy interactions
 
 #GESDIB_A
 #on SEX_A=2
@@ -38,6 +36,11 @@ premium_data <- premium_data %>% select(AGEP_A, SEX_A, RACEALLP_A, HISPALLP_A, M
 
 #DRK12MWK_A
 #on DRKLIFE_A=1
+
+#hard interactions?
+
+#EMDOCCUPN2_A,EMDOCCUPN1_A,EMDINDSTN1_A
+#on EMPLASTWK_A=1 or EMPNOWRK_A=1 or EMPWHYNOT_A=7 or EMPWHENWRK_A=1
 
 #MCPART_A
 #on MEDICARE_A in (1,2)
@@ -52,38 +55,38 @@ premium_data <- premium_data %>% select(AGEP_A, SEX_A, RACEALLP_A, HISPALLP_A, M
 #on PRIVATE_A in (1,2)
 
 #making invalid values NA (refused, not ascertained, don't know)
-premium_data[premium_data$AGEP_A %in% c(97, 98, 99)] <- NA
-premium_data[premium_data$SEX_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$RACEALLP_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$HISPALLP_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$MARITAL_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$NATUSBORN_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$ORIENT_A %in% c(7,8)] <- NA
-premium_data[premium_data$RATCAT_A %in% c(98)] <- NA
-premium_data[premium_data$EDUCP_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$EMDOCCUPN2_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$EMDOCCUPN2_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$ACCSSINT_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$EMDINDSTN1_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$NATUSBORN_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$DIBEV_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$PREDIB_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$GESDIB_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$ASEV_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$LONGCOVD2_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$EVERCOVD_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$ANXFREQ_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$DEPFREQ_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$SMOKELSEV1_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$SMKCIGST_A %in% c(9)] <- NA
-premium_data[premium_data$DRKLIFE_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$DRK12MWK_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$DRKSTAT_A %in% c(10)] <- NA
-premium_data[premium_data$MODFREQW_A %in% c(97,98,99)] <- NA
-premium_data[premium_data$MCPART_A %in% c(7,8,9)] <- NA
-premium_data[premium_data$MCCHOICE_A %in% c(7)] <- NA
-premium_data[premium_data$EXCHANGE_A %in% c(8)] <- NA
-premium_data[premium_data$PLNWRKR1_A %in% c(97,98,99)] <- NA
+premium_data$AGEP_A[premium_data$AGEP_A %in% c(97, 98, 99)] <- NA
+premium_data$SEX_A[premium_data$SEX_A %in% c(7,8,9)] <- NA
+premium_data$RACEALLP_A[premium_data$RACEALLP_A %in% c(7,8,9)] <- NA
+premium_data$HISPALLP_A[premium_data$HISPALLP_A %in% c(97,98,99)] <- NA
+premium_data$MARITAL_A[premium_data$MARITAL_A %in% c(7,8,9)] <- NA
+premium_data$NATUSBORN_A[premium_data$NATUSBORN_A %in% c(7,8,9)] <- NA
+premium_data$ORIENT_A[premium_data$ORIENT_A %in% c(7,8)] <- NA
+premium_data$RATCAT_A[premium_data$RATCAT_A %in% c(98)] <- NA
+premium_data$EDUCP_A[premium_data$EDUCP_A %in% c(97,98,99)] <- NA
+premium_data$EMDOCCUPN2_A[premium_data$EMDOCCUPN2_A %in% c(97,98,99)] <- NA
+premium_data$EMDOCCUPN1_A[premium_data$EMDOCCUPN1_A %in% c(97,98,99)] <- NA
+premium_data$ACCSSINT_A[premium_data$ACCSSINT_A %in% c(7,8,9)] <- NA
+premium_data$EMDINDSTN1_A[premium_data$EMDINDSTN1_A %in% c(97,98,99)] <- NA
+premium_data$NATUSBORN_A[premium_data$NATUSBORN_A %in% c(7,8,9)] <- NA
+premium_data$DIBEV_A[premium_data$DIBEV_A %in% c(7,8,9)] <- NA
+premium_data$PREDIB_A [premium_data$PREDIB_A %in% c(7,8,9)] <- NA
+premium_data$GESDIB_A[premium_data$GESDIB_A %in% c(7,8,9)] <- NA
+premium_data$ASEV_A[premium_data$ASEV_A %in% c(7,8,9)] <- NA
+premium_data$LONGCOVD2_A [premium_data$LONGCOVD2_A %in% c(7,8,9)] <- NA
+premium_data$EVERCOVD_A[premium_data$EVERCOVD_A %in% c(7,8,9)] <- NA
+premium_data$ANXFREQ_A[premium_data$ANXFREQ_A %in% c(7,8,9)] <- NA
+premium_data$DEPFREQ_A[premium_data$DEPFREQ_A %in% c(7,8,9)] <- NA
+premium_data$SMOKELSEV1_A[premium_data$SMOKELSEV1_A %in% c(7,8,9)] <- NA
+premium_data$SMKCIGST_A[premium_data$SMKCIGST_A %in% c(9)] <- NA
+premium_data$DRKLIFE_A[premium_data$DRKLIFE_A %in% c(7,8,9)] <- NA
+premium_data$DRK12MWK_A[premium_data$DRK12MWK_A %in% c(97,98,99)] <- NA
+premium_data$DRKSTAT_A[premium_data$DRKSTAT_A %in% c(10)] <- NA
+premium_data$MODFREQW_A[premium_data$MODFREQW_A %in% c(97,98,99)] <- NA
+premium_data$MCPART_A[premium_data$MCPART_A %in% c(7,8,9)] <- NA
+premium_data$MCCHOICE_A[premium_data$MCCHOICE_A %in% c(7)] <- NA
+premium_data$EXCHANGE_A[premium_data$EXCHANGE_A %in% c(8)] <- NA
+premium_data$PLNWRKR1_A[premium_data$PLNWRKR1_A %in% c(97,98,99)] <- NA
 
 #non-categorical: AGEP_A,POVRATTC_A,PPSU, PSTRAT, WTFA_A
 
@@ -98,7 +101,15 @@ nrow(premium_data)
 
 #create linear model
 
-premium_model <- lm(HICOSTR1_A ~ ., data=premium_data)
+premium_model <- lm(HICOSTR1_A ~ AGEP_A + RACEALLP_A + HISPALLP_A + MARITAL_A + NATUSBORN_A + ORIENT_A + #demographic
+                    POVRATTC_A + RATCAT_A + EDUCP_A + ACCSSINT_A + EMDINDSTN1_A + #socio-economic
+                    DIBEV_A + PREDIB_A + GESDIB_A + HYPEV_A + ASEV_A + ANXFREQ_A + DEPFREQ_A + #health conditions
+                    SMOKELSEV1_A + SMKCIGST_A + DRKSTAT_A + MODFREQW_A + #health related behaviors
+                    REGION + URBRRL23 + #geography
+                    MCPART_A + MCCHOICE_A + EXCHANGE_A + PLNWRKR1_A + #access to care
+                    PPSU + PSTRAT + WTFA_A, 
+                    #SEX_A * GESDIB_A + LONGCOVD2_A * EVERCOVD_A + DRKLIFE_A * DRK12MWK_A, #interactions: EMDOCCUPN2_A,EMDOCCUPN1_A,EMDINDSTN1_A,MCPART_A,MCCHOICE_A,EXCHANGE_A,PLNWRKR1_A
+                    data=premium_data)
 
 summary(premium_model)
 
